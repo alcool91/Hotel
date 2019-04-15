0f7048fa314a6cb381180603abeacaab35e1c5cf
brentmw1 <brent.wickenheiser@rockets.utoledo.edu>
2019-04-15 11:50:35 -0400

    Very start of reports

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Report.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+public class Report {
+    private double baseRate;
+    private short totalReservations;
+    private short prepaidNum;
+    private short sixtyDayNum;
+    private short conventionalNum;
+    private short incentiveNum;
+
+    public Report() {
+        baseRate = 100;
+        totalReservations = 0;
+        prepaidNum = 0;
+        sixtyDayNum = 0;
+        conventionalNum = 0;
+        incentiveNum = 0;
+    }
+
+    public Report(Calendar[] dates, Reservation[] reservations, string s)
+    {
+        foreach (Calendar date in dates) {
+
+        }
+
+    }
+
+    string expectedOccupancy()
+    {
+        return "this";
+    }
+
+    
+}
+
